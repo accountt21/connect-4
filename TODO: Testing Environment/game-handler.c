504f630be0fe9aa15febfa55f48e296c1769d65f
Allie <allie.g.saoud@gmail.com>
2025-11-23 23:31:27 +0200

    Delete game-handler.c

--- TODO: Testing Environment/game-handler.c
+++
@@ -1,9 +0,0 @@
-/*
-This is supposed to run the supposed perfect bots against
-   
-   1.     each other
-   3.     random bots
-   2.     other imperfect but good bots
-
-and analyze winning rates and ensure it fits with the theoretical expectations.
-*/
